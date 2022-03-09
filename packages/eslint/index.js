91239de58d33681b2fc0b5e40eda76e6da8f65f9
Jake Lazaroff <yo@jake.nyc>
2022-03-09 00:04:56 -0500

    Remove ban-ts-comment eslint override

--- packages/eslint/index.js
+++ packages/eslint/index.js
@@ -1,22 +1,17 @@
 module.exports = {
-  "env": { "node": true },
-  "plugins": ["react", "@typescript-eslint", "jsx-a11y"],
-  "extends": [
+  env: { node: true },
+  plugins: ["react", "@typescript-eslint", "jsx-a11y"],
+  extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:jsx-a11y/recommended"
   ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaFeatures": { "jsx": true },
-    "ecmaVersion": "latest",
-    "sourceType": "module"
-  },
-  "rules": {
-    "ban-ts-comment": {
-      "ts-ignore": "allow-with-description"
-    }
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: "latest",
+    sourceType: "module"
   }
-}
+};
