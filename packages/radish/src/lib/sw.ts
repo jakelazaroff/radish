1aed166fe8a3dd0cdab54d7187321ad8380b1d06
Jake Lazaroff <yo@jake.nyc>
2022-03-14 21:50:50 -0400

    Split service worker cache

--- packages/radish/src/lib/sw.ts
+++ packages/radish/src/lib/sw.ts
@@ -1,37 +1,29 @@
 /// <reference lib="webworker" />
 
-// https://web.archive.org/web/20201107234110/https://phyks.me/2019/01/manage-expiration-of-cached-assets-with-service-worker-caching.html
-// https://gomakethings.com/offline-first-with-service-workers-and-vanilla-js/
-// https://gomakethings.com/saving-recently-viewed-pages-offline-with-service-workers-and-vanilla-js/
-// https://gomakethings.com/writing-your-first-service-worker-with-vanilla-js/
-// https://gomakethings.com/how-to-set-an-expiration-date-for-items-in-a-service-worker-cache/
-// https://gomakethings.com/the-amazing-power-of-service-workers/
-// https://jakearchibald.com/2014/offline-cookbook/
-// https://adactio.com/journal/13821
-
 export type {};
 declare const self: ServiceWorkerGlobalScope;
 declare const PUBLIC_PATH: string;
 
-const CACHE_NAME = "v1";
+const VERSION = "v1";
+const PAGES_CACHE = `pages_${VERSION}`;
+const ASSET_CACHE = `assets_${VERSION}`;
+const CACHES = new Set([PAGES_CACHE, ASSET_CACHE]);
 
 const SW_CACHE_KEY = "sw-cache-expires";
 const DEFAULT_MAX_AGE = 0;
 const DEFAULT_PUBLIC_MAX_AGE = 31_557_600;
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(["/"]);
-    })
-  );
-});
-
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(keyList.map(key => caches.delete(key)));
-    })
+    caches
+      .keys()
+      .then(keys => {
+        const deletes = keys
+          .filter(key => !CACHES.has(key))
+          .map(key => caches.delete(key));
+        return Promise.all(deletes);
+      })
+      .then(() => self.clients.claim())
   );
 });
 
@@ -41,6 +33,9 @@ self.addEventListener("fetch", event => {
       const expires = Number(cached?.headers.get(SW_CACHE_KEY));
       if (cached && new Date().getTime() / 1000 < expires) return cached;
 
+      const url = new URL(event.request.url);
+      const isPublic = url.pathname.startsWith(PUBLIC_PATH);
+
       const fresh = fetch(event.request)
         .then(res => {
           if (!res || res.status !== 200 || res.type !== "basic") return res;
@@ -49,8 +44,7 @@ self.addEventListener("fetch", event => {
           if (control["no-store"]) return res;
 
           // calculate the new expiry time
-          const url = new URL(event.request.url);
-          const defaultMaxAge = url.pathname.startsWith(PUBLIC_PATH)
+          const defaultMaxAge = isPublic
             ? DEFAULT_PUBLIC_MAX_AGE
             : DEFAULT_MAX_AGE;
           const maxAge = control["max-age"] ?? defaultMaxAge,
@@ -59,7 +53,7 @@ self.addEventListener("fetch", event => {
           // clone and cache the response
           const clone = res.clone();
           const headers = [...clone.headers, [SW_CACHE_KEY, "" + expires]];
-          caches.open(CACHE_NAME).then(cache => {
+          caches.open(isPublic ? ASSET_CACHE : PAGES_CACHE).then(cache => {
             cache.put(event.request.url, new Response(clone.body, { headers }));
           });
 
